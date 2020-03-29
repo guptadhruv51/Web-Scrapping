0387979d1f71276c40962df7a83df37d2aa82719
Dhruv Gupta <guptadhruv51@gmail.com>
2020-03-29 21:37:14 +0530

    Connected to sqlite

--- Scrapy Tutorial/quotetutorial/quotetutorial/pipelines.py
+++ Scrapy Tutorial/quotetutorial/quotetutorial/pipelines.py
@@ -4,8 +4,32 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sqlite3
 
 class QuotetutorialPipeline(object):
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn=sqlite3.connection('myquotes.db')
+        self.curr=self.conn.cursor()
+
+    def create_table(self,item):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+        self.curr.execute("""create table quotes_tb
+        ( title text,
+          author text,
+          tags text
+        )""")
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+    def store_db(self,item):
+        self.curr.execute("""insert into table quotes_tb values(?,?,?)""",(
+            item['title'][0],
+            item['author'][0],
+            item['tags'][0]
+        ))
+        self.conn.commit()
