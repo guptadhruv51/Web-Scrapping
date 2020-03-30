ffee7e033ed96e5b874826dbb9ba908d08d5c761
Dhruv Gupta <guptadhruv51@gmail.com>
2020-03-30 22:42:28 +0530

    Scrapping Multiple Pages

---
+++ Scrapy Tutorial/Multiple_pages/multiplepages/multiplepages/spiders/multiplepages.py
@@ -0,0 +1,22 @@
+import scrapy
+from ..items import MultiplepagesItem
+
+class Multiplepages(scrapy.Spider):
+    name='quotes'
+    start_urls=[
+        'http://quotes.toscrape.com/'
+    ]
+    def parse(self,response):
+        items=MultiplepagesItem()
+        all_div_quotes=response.css('div.quote')
+        for quote in all_div_quotes:
+            title = quote.css('span.text::text').extract()
+            author=quote.css('.author::text').extract()
+            tag=quote.css('.tag::text').extract()
+            items['title']=title
+            items['author']=author
+            items['tags']=tag
+            yield items
+        next_page=response.css('li.next a').xpath('@href').extract()
+        if next_page is not None:
+            yield response.follow(next_page,callback=self.parse)
