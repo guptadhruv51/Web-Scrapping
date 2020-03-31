d2ed6d8a9d4711ac949f8a380ddcf0bf9adc426b
Dhruv Gupta <guptadhruv51@gmail.com>
2020-03-31 18:04:25 +0530

    Pagination

---
+++ Scrapy Tutorial/Pagination/Pagination/Pagination/spiders/pagination.py
@@ -0,0 +1,27 @@
+import scrapy
+from ..items import PaginationItem
+
+class PageItem(scrapy.Spider):
+    name='quotes'
+    page_number=2
+    start_urls=[
+        'http://quotes.toscrape.com/page/1/'
+    ]
+
+    def parse(self,response):
+        items=PaginationItem()
+        all_div_quotes=response.css('div.quote')
+        for quotes in all_div_quotes:
+            title=quotes.css('span.text::text').extract()
+            author=quotes.css('.author.text::text').extract()
+            tags=quotes.css('.tag::text').extract()
+
+            items['title']=title
+            items['author']=author
+            items['tags']=tags
+
+            yield items
+    next_page='http://quotes.toscrape.com/page/'+str(PaginationItem.page_number)+'/'
+    if PaginationItem.page_number <11:
+        PaginationItem.page_number +=1
+        yield response.follow(next_page , callback=self.parse)
